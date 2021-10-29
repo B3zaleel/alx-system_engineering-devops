27c434845e22d5b499ae5ef773fed1e386c747f5
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-10-29 22:24:01 +0100

    :wrench: Updated script

---
+++ 0x05-processes_and_signals/foo.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+if [ "$#" -ne 1 ]; then
+    echo 'Usage: manage_my_process {start|stop|restart}'
+elif [ "$1" = "start" ]; then
+    ./manage_my_process &
+    echo "$!" > /var/run/my_process.pid
+    echo "manage_my_process started"
+elif [ "$1" = "stop" ]; then
+    if [ -f /var/run/my_process.pid ]; then
+        kill "$(cat /var/run/my_process.pid )"
+    fi
+    if [ -f /var/run/my_process.pid ]; then
+       rm /var/run/my_process.pid
+    fi
+    echo "manage_my_process stopped"
+elif [ "$1" = "restart" ]; then
+    if [ -f /var/run/my_process.pid ]; then
+        kill "$(cat /var/run/my_process.pid )"
+    fi
+    if [ -f /var/run/my_process.pid ]; then
+       rm /var/run/my_process.pid
+    fi
+    ./manage_my_process  &
+    echo "$!" > /var/run/my_process.pid
+    echo "manage_my_process restarted"
+else echo 'Usage: manage_my_process {start|stop|restart}'
+fi
