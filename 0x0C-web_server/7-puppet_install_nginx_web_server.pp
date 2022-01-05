fb50052befdb8b574f3cc9c95502f2753a978503
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-01-05 18:15:19 +0100

    :wrench: Update manifest

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -15,7 +15,7 @@ file { 'The home page':
   path    => '/var/www/html/index.html',
   mode    => '0744',
   owner   => 'www-data',
-  content => 'Hellow World\n'
+  content => 'Hello World'
 }
 
 file { 'The 404 page':
