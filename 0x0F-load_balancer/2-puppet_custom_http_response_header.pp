2b61411e2559547831fd40cb0492004dba16cfd4
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-01-10 19:05:16 +0100

    :wrench: Update server setup manifest

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,65 +1,52 @@
 # Nginx web server setup and configuration
-# Software installations
-class software_resources {
-  # exec { 'Update the apt repository':
-  #   command => 'apt-get -q update',
-  #   path    => '/usr/bin:/usr/sbin:/bin'
-  # }
-  exec { 'Install the web server':
-    command => 'apt-get -q -y install nginx',
-    path    => '/usr/bin:/usr/sbin:/bin'
-  }
+package { 'nginx':
+  ensure   => present,
+  provider => 'apt'
 }
 
-# The static files
-class static_files {
-  # resources
-  file { 'The static files directory':
-    ensure => directory,
-    path   => '/var/www/',
-    mode   => '0666',
-    owner  => 'www-data'
-  }
-
-  file { 'The static files HTML pages directory':
-    ensure => directory,
-    path   => '/var/www/html',
-    mode   => '0666',
-    owner  => 'www-data'
-  }
-
-  file { 'The static files error pages directory':
-    ensure => directory,
-    path   => '/var/www/error',
-    mode   => '0666',
-    owner  => 'www-data'
-  }
-
-  file { 'The home page':
-    ensure  => file,
-    path    => '/var/www/html/index.html',
-    mode    => '0666',
-    owner   => 'www-data',
-    content => "Hello World!\n"
-  }
-
-  file { 'The 404 page':
-    ensure  => file,
-    path    => '/var/www/error/404.html',
-    mode    => '0666',
-    owner   => 'www-data',
-    content => "Ceci n'est pas une page\n"
-  }
+file { 'The static files directory':
+  ensure => directory,
+  path   => '/var/www/',
+  mode   => '0666',
+  owner  => 'www-data'
 }
 
-# configuration files
-class config_files {
-  file { 'Nginx server config file':
-    ensure  => file,
-    path    => '/etc/nginx/sites-enabled/default',
-    mode    => '0666',
-    owner   => 'www-data',
-    content =>
+file { 'The static files HTML pages directory':
+  ensure => directory,
+  path   => '/var/www/html',
+  mode   => '0666',
+  owner  => 'www-data'
+}
+
+file { 'The static files error pages directory':
+  ensure => directory,
+  path   => '/var/www/error',
+  mode   => '0666',
+  owner  => 'www-data'
+}
+
+file { 'The home page':
+  ensure  => file,
+  path    => '/var/www/html/index.html',
+  mode    => '0666',
+  owner   => 'www-data',
+  content => "Hello World!\n"
+}
+
+file { 'The 404 page':
+  ensure  => file,
+  path    => '/var/www/error/404.html',
+  mode    => '0666',
+  owner   => 'www-data',
+  content => "Ceci n'est pas une page\n"
+}
+
+file { 'Nginx server config file':
+  ensure  => file,
+  path    => '/etc/nginx/sites-enabled/default',
+  mode    => '0666',
+  owner   => 'www-data',
+  content =>
 "server {
 	listen 80 default_server;
 	listen [::]:80 default_server;
@@ -86,17 +73,8 @@ class config_files {
 	}
 }
 "
-  }
 }
 
-# Essential processes
-class essential_services {
-  service { 'nginx':
-    ensure => running
-  }
+service { 'nginx':
+  ensure => running
 }
-
-include software_resources
-include static_files
-include config_files
-include essential_services
