45ae0274356c34511a3f233966f8870563613e6a
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-01-10 20:58:36 +0100

    :wrench: Update server setup manifest

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -13,22 +13,57 @@ exec { 'install-nginx':
 file { 'Home-Page':
   ensure  => file,
   path    => '/var/www/html/index.html',
-  content => "Hello World!\n"
+  mode    => '0666',
+  owner   => 'www-data',
+  content => 'Hello World!'
 }
 
-file { '404-Page':
+# file { '404-Page':
+#   ensure  => file,
+#   path    => '/var/www/error/404.html',
+#   mode    => '0666',
+#   owner   => 'www-data',
+#   content => "Ceci n'est pas une page\n"
+# }
+
+file { 'Nginx-Config':
   ensure  => file,
-  path    => '/var/www/html/404.html',
-  content => "Ceci n'est pas une page\n"
+  path    => '/etc/nginx/sites-enabled/default',
+  mode    => '0666',
+  owner   => 'www-data',
+  content =>
+"server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+
+	root /var/www/html;
+	index index.html index.nginx-debian.html;
+
+	server_name _;
+
+	location / {
+		try_files \$uri \$uri/ =404;
+		add_header X-Served-By \$hostname;
+	}
+
+	if (\$request_filename ~ redirect_me){
+		rewrite ^ https://sketchfab.com/bluepeno/models permanent;
+	}
+
+	error_page 404 /404.html;
+	location = /404.html {
+		root /var/www/error/;
+		internal;
+		add_header X-Served-By \$hostname;
+	}
+}
+",
+  require => [File['Home-Page']]
 }
 
-file_line { 'Nginx-Config':
-  ensure  => present,
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'server_name _;',
-  line    => "\tadd_header X-Served-By \$hostname;",
-  require => [File['Home-Page'], File['404-Page'], Exec['install-nginx']]
+service { 'nginx':
+  ensure  => running,
+  require => [Exec['install-nginx'], File['Nginx-Config']]
 }
--> service { 'nginx':
-  ensure  => running
-}
+
+Service['nginx']
