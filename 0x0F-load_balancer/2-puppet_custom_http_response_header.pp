1d4e369783572aa5a1b863892e58afb28aa2b186
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-01-10 21:21:21 +0100

    :wrench: Update server setup manifest

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -9,19 +9,19 @@ package { 'nginx':
   require => Exec['update-apt']
 }
 
-file_line { 'Custom-Header':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'listen 80 default_server;',
-  line    => 'add_header X-Served-By $hostname;',
-  require => Package['nginx'],
-}
-
 file_line { 'Redirection':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server;',
   line    => 'rewrite ^/redirect_me https://sketchfab.com/bluepeno/models permanent;',
+  require => Package['nginx'],
+}
+
+file_line { 'Custom-Header':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $hostname;',
   require => Package['nginx'],
 }
 
