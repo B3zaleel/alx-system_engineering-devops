2f37d5c42c55d3e12f8e4edeb3284eb93c451cff
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-01-10 20:59:31 +0100

    :wrench: Update server setup manifest

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,69 +1,35 @@
 # Nginx web server setup and configuration
 exec { 'apt-get-update':
-  command => 'bash -c "apt-get update; echo"',
-  path    => '/usr/bin:/usr/sbin:/bin'
+  command => '/usr/bin/apt-get update',
 }
 
-exec { 'install-nginx':
-  command => 'bash -c "apt-get -y -q install nginx; echo"',
-  path    => '/usr/bin:/usr/sbin:/bin',
-  require => Exec['apt-get-update']
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['apt-get-update'],
 }
 
-file { 'Home-Page':
-  ensure  => file,
-  path    => '/var/www/html/index.html',
-  mode    => '0666',
-  owner   => 'www-data',
-  content => 'Hello World!'
+file_line { 'a':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => Package['nginx'],
 }
 
-# file { '404-Page':
-#   ensure  => file,
-#   path    => '/var/www/error/404.html',
-#   mode    => '0666',
-#   owner   => 'www-data',
-#   content => "Ceci n'est pas une page\n"
-# }
-
-file { 'Nginx-Config':
-  ensure  => file,
-  path    => '/etc/nginx/sites-enabled/default',
-  mode    => '0666',
-  owner   => 'www-data',
-  content =>
-"server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-
-	root /var/www/html;
-	index index.html index.nginx-debian.html;
-
-	server_name _;
-
-	location / {
-		try_files \$uri \$uri/ =404;
-		add_header X-Served-By \$hostname;
-	}
-
-	if (\$request_filename ~ redirect_me){
-		rewrite ^ https://sketchfab.com/bluepeno/models permanent;
-	}
-
-	error_page 404 /404.html;
-	location = /404.html {
-		root /var/www/error/;
-		internal;
-		add_header X-Served-By \$hostname;
-	}
+file_line { 'b':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
 }
-",
-  require => [File['Home-Page']]
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+  require => Package['nginx'],
 }
 
 service { 'nginx':
   ensure  => running,
-  require => [Exec['install-nginx'], File['Nginx-Config']]
+  require => Package['nginx'],
 }
-
-Service['nginx']
