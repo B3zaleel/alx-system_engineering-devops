ff3f7a4353025d9b484b0da3e87e91ec5a85485d
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-02-16 14:18:03 +0100

    Fix styling issues

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,6 @@
 # Fixes a faulty wordpress site
 exec { 'fix-wordpress':
-  command => [
-    '/usr/bin/bash -c',
-    "'sed -i",
-    's/class-wp-locale.phpp/class-wp-locale.php/',
-    '/var/www/html/wp-settings.php',
-    "; service apache2 restart'",
-  ].join(' ')
+  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
+/var/www/html/wp-settings.php; service apache2 restart"',
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
