23904d08e10d5e4ed7146dbe561dfa04ec6a1e02
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-02-24 12:27:00 +0100

    Update puppet manifest

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # Fixes an nginx site that can't handle multiple concurrent requests
 exec { 'fix--for-nginx':
   command => 'bash -c "sed -iE s/#\s*multi_accept on;/multi_accept on;/ \
-/etc/nginx/nginx.conf; sed -iE s/worker_processes [a-z0-9]*/worker_processes 15/ \
+/etc/nginx/nginx.conf; sed -iE s/worker_processes [a-z0-9]*/worker_processes 126/ \
 /etc/nginx/nginx.conf; service nginx restart"',
   path    => '/usr/bin:/usr/sbin:/bin'
 }
